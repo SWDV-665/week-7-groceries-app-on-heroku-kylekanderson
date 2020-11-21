3068d0e802436477cef6f59ba77b2e1d7a132adf
Kyle Anderson <kylea@schwebdesign.com>
2020-11-21 08:33:05 -0500

    initial commit::

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,6 +3,8 @@ import { ToastController } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
 import { GroceriesService } from '../../services/groceries.service';
 import { InputDialogService } from '../../services/input-dialog.service';
+import { Plugins } from '@capacitor/core';
+const { Share } = Plugins;
 
 
 @Component({
@@ -34,6 +36,21 @@ export class HomePage {
     toast.present();
 
     this.dataService.removeItem(index);
+  }
+
+  async shareItem(item, index) {
+    console.log('sharing item: ', item, index);
+    const toast = await this.toastController.create({
+      message: 'Sharing Item - ' + item.name + '...',
+      duration: 2000
+    });
+    let shareRet = await Share.share({
+      title: 'Shared via Groceries App',
+      text: 'Grocery Item - Name: ' + item.name + ' - Quantity: ' + item.quantity,
+      dialogTitle: 'Share'
+    })
+      .then(() => console.log('Successful share'))
+      .catch((error) => console.error('Error sharing', error));
   }
 
   async editItem(item, index) {
