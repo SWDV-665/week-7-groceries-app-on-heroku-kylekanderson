f58f3ec1806c2fc50e75c04ba913dbc42fe5e44b
Kyle Anderson <kylea@schwebdesign.com>
2020-11-27 08:39:51 -0500

    initial commit

--- src/app/services/groceries.service.ts
+++ src/app/services/groceries.service.ts
@@ -1,16 +1,54 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroceriesService {
 
-    items = [];
+    items: any = [];
 
-  constructor() { }
+    dataChanged$: Observable<boolean>;
 
-  getItems() {
-    return this.items;
+    private dataChangeSubject: Subject<boolean>;
+
+    baseUrl = 'http://localhost:8080';
+
+  constructor(
+    public http: HttpClient
+  ) {
+    console.log('Hello Groceries Service');
+
+    this.dataChangeSubject = new Subject<boolean>();
+    this.dataChanged$ = this.dataChangeSubject.asObservable();
+  }
+
+  getItems(): Observable<object[]> {
+    return this.http.get(this.baseUrl + '/api/groceries').pipe(
+      map(this.extractData),
+      catchError(this.handleError)
+    );
+  }
+
+  private extractData(res: Response) {
+    let body = res.body;
+    return body || {};
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const err = error || '';
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    }
+    else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
   removeItem(index) {
